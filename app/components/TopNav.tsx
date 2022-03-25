48a34b3ac86433333c4fed1dc8ad6b255fe46da2
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-03-25 16:24:34 -0700

    Add login dropdown

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -3,13 +3,15 @@ import { ThemeButton } from './ThemeButton';
 import { getFolder } from '~/utils';
 import classNames from 'classnames';
 import { useConfig } from './ConfigProvider';
-import { useContext } from 'react';
+import { Fragment, useContext } from 'react';
 import { UiContext } from './UiStateProvider';
+import { Menu, Transition } from '@headlessui/react';
 
 export function TopNav() {
   const config = useConfig();
   const [state, setState] = useContext(UiContext);
   const { logo, logoText, sections, actions, name } = config?.site ?? {};
+  console.log('actions', actions);
   return (
     <div className="bg-stone-700 p-3 px-8 fixed w-screen top-0 z-30">
       <nav className="flex items-center justify-between flex-wrap max-w-[1440px] mx-auto">
@@ -48,22 +50,56 @@ export function TopNav() {
           </Link>
         </div>
         <div className="block xl:hidden">
-          <button className="flex items-center px-3 py-2 text-stone-200 hover:text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
+          <Menu as="div" className="ml-3 relative">
+            <div>
+              <Menu.Button className="bg-transparent flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                <span className="sr-only">Open user menu</span>
+                <div className="flex items-center px-3 py-2 text-stone-200 hover:text-white">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                    />
+                  </svg>
+                </div>
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-              />
-            </svg>
-          </button>
+              <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                {actions?.map((action) => (
+                  <Menu.Item key={action.url}>
+                    {({ active }) => (
+                      <a
+                        href={action.url}
+                        className={classNames(
+                          active ? 'bg-gray-100' : '',
+                          'block px-4 py-2 text-sm text-gray-700',
+                        )}
+                      >
+                        {action.title}
+                      </a>
+                    )}
+                  </Menu.Item>
+                ))}
+              </Menu.Items>
+            </Transition>
+          </Menu>
         </div>
 
         <div className="hidden flex-grow xl:flex items-center w-auto">

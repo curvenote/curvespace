113d9793177e8f630c973c2eb481c4a9b0fe21f9
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-04-04 15:16:49 -0700

    Use HeroIcon icon component

--- app/components/renderers/code.tsx
+++ app/components/renderers/code.tsx
@@ -7,6 +7,8 @@ import { useTheme } from '../theme';
 import { useEffect, useRef, useState } from 'react';
 import { copyTextToClipboard } from '~/utils';
 import classNames from 'classnames';
+import { CheckIcon } from '@heroicons/react/outline';
+import { DuplicateIcon } from '@heroicons/react/solid';
 
 type Props = {
   children: string;
@@ -69,9 +71,9 @@ const code: NodeRenderer<Code> = (node: any) => {
       <div className="absolute hidden top-1 right-1 group-hover:block z-10">
         <button
           className={classNames(
-            'p-1 cursor-pointer transition-color duration-200 ease-in-out border rounded-md',
+            'p-1 cursor-pointer transition-color duration-200 ease-in-out',
             {
-              'text-primary-500 border-primary-500 ': !showCopied,
+              'text-primary-500 border-primary-500': !showCopied,
               'text-success border-success ': showCopied,
             },
           )}
@@ -84,15 +86,11 @@ const code: NodeRenderer<Code> = (node: any) => {
               });
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z" />
-            <path d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z" />
-          </svg>
+          {showCopied ? (
+            <CheckIcon className="w-5 h-5" />
+          ) : (
+            <DuplicateIcon className="w-5 h-5" />
+          )}
         </button>
       </div>
       <CodeBlock

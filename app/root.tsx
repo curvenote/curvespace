27056d6cd980c2e03eab1d8df9162ab26a33999e
Rowan Cockett <rowanc1@gmail.com>
2022-05-31 11:18:02 -0600

    🛤  Analytics for google

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,7 @@ import tailwind from './styles/app.css';
 import { getMetaTagsForSite, getConfig, SiteManifest } from './utils';
 import { ConfigProvider } from './components/ConfigProvider';
 import { UiStateProvider } from './components/UiStateProvider';
+import { Analytics } from './components/analytics';
 
 export const meta: MetaFunction = ({ data }) => {
   return getMetaTagsForSite({
@@ -63,13 +64,7 @@ function Document({
         {title && <title>{title}</title>}
         <Meta />
         <Links />
-        {config?.analytics?.plausible && (
-          <script
-            defer
-            data-domain={config.analytics.plausible}
-            src="https://plausible.io/js/plausible.js"
-          ></script>
-        )}
+        <Analytics analytics={config?.analytics} />
       </head>
       <body className="m-0 transition-colors duration-500 bg-white dark:bg-stone-900">
         <UiStateProvider>

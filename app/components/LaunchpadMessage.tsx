5c2d3040707b254d754631b778b5b4150227de00
Rowan Cockett <rowanc1@gmail.com>
2022-06-23 08:19:56 -0600

    📖 Update copy

--- app/components/LaunchpadMessage.tsx
+++ app/components/LaunchpadMessage.tsx
@@ -5,7 +5,7 @@ export default function LaunchpadMessage() {
   if (typeof document === 'undefined') return null;
   const hostname = window.location.hostname;
   const [expiresIn, setExpiresIn] = useState('and will expire in - days.');
-  const [show, setShow] = useState(window.sessionStorage.getItem(hostname) !== 'hide');
+  const [show, setShow] = useState(true);
 
   const match = hostname.match(/^launchpad-(.*).curve.space$/);
 
@@ -26,46 +26,64 @@ export default function LaunchpadMessage() {
     });
   }, []);
 
-  const clickDismiss = () => {
-    window.sessionStorage.setItem(hostname, 'hide');
-    setShow(false);
-  };
-
   if (!show || match == null) return null;
 
   return (
-    <div className="fixed border text-sm bg-white bottom-2 right-2 p-2 w-[385px] z-20 text-center space-y-2 launchpad-message shadow animate-fadein-fast">
-      <p className="mb-1 text-lg font-semibold">🚀 Try Curvenote 🚀</p>
+    <div className="fixed border text-sm bg-white bottom-2 right-2 p-3 w-[385px] max-w-full z-20 space-y-2 shadow animate-fadein-fast">
+      <p className="mb-1 text-lg font-semibold text-center">🚀 Try Curvenote 🚀</p>
       <p>
         This website was launched using{' '}
-        <a href="https://try.curvenote.com">try.curvenote.com</a> {expiresIn}
+        <a
+          href="https://try.curvenote.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="hover:underline text-blue-500"
+        >
+          try.curvenote.com
+        </a>{' '}
+        {expiresIn}
       </p>
-      <div className="text-left px-2 space-y-1">
-        <p>What&apos;s Next?</p>
+      <div className="text-left space-y-1">
+        <p className="font-bold">What&apos;s Next?</p>
         <ul className="list-inside indent-4">
           <li>
-            <a href="https://try.curvenote.com" target="_blank" rel="noreferrer">
-              Launch temporary website like this one
-            </a>
-          </li>
-          <li>
-            Is this yours?{' '}
             <a
               href="https://docs.curvenote.com/web/launchpad#J7q8FCSPRp"
               target="_blank"
-              rel="noreferrer"
+              rel="noreferrer noopener"
+              className="hover:underline text-blue-500"
             >
-              Customize it and make it permanent
+              Customize the site and make it permanent
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://try.curvenote.com"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="hover:underline text-blue-500"
+            >
+              Launch temporary website like this one
             </a>
           </li>
         </ul>
       </div>
-      <button
-        className="border p-1 px-2 my-4 rounded text-blue-500 hover:text-blue-600 hover:border-blue-500"
-        onClick={clickDismiss}
-      >
-        Dismiss
-      </button>
+      <div className="text-right py-2">
+        <a
+          className="border p-2 mr-2 rounded text-blue-500 hover:text-blue-600 hover:border-blue-500 cursor-pointer"
+          onClick={() => setShow(false)}
+        >
+          Dismiss
+        </a>
+        <a
+          className="border p-2 rounded text-white bg-blue-400 hover:bg-blue-500 hover:border-blue-400"
+          href="https://docs.curvenote.com/web/launchpad#J7q8FCSPRp"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <span className="text-white">Learn More</span>
+        </a>
+      </div>
     </div>
   );
 }
